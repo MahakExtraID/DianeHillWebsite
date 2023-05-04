3fec31fa28b1b48ae6f657790b3b3ddd57b9c16d
MahakExtraID <132599750+MahakExtraID@users.noreply.github.com>
2023-05-04 12:36:49 -0400

    main files added to the website

---
+++ main.js
@@ -0,0 +1,35 @@
+// JavaScript Functions for Popup Box 1
+function showPopup1() {
+  document.querySelector("#popup1").style.display = "block";
+}
+
+function closePopup1() {
+  document.querySelector("#popup1").style.display = "none";
+}
+
+// JavaScript Functions for Popup Box 2
+function showPopup2() {
+  document.querySelector("#popup2").style.display = "block";
+}
+
+function closePopup2() {
+  document.querySelector("#popup2").style.display = "none";
+}
+
+// JavaScript Functions for Popup Box 3
+function showPopup3() {
+  document.querySelector("#popup3").style.display = "block";
+}
+
+function closePopup3() {
+  document.querySelector("#popup3").style.display = "none";
+}
+
+// JavaScript Functions for Popup Box 4
+function showPopup4() {
+  document.querySelector("#popup4").style.display = "block";
+}
+
+function closePopup4() {
+  document.querySelector("#popup4").style.display = "none";
+}
